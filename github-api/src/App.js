c167860f178b76e4351f086bb8e7955051501ac2
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-06-27 09:53:58 +1000

    render options to pick from

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -3,38 +3,91 @@ import './App.css';
 import RepoNames from './components/RepoNames';
 
 
+
+const names = [
+              'alfredosorio',
+              'ifollowedthewhiterabbit',
+              'Livia-Santos',
+              'Jay-Aye',
+              'Chris-Robertson',
+              'CharleyFarley',
+              'george2kra',
+              'teck7',
+              'Reynolds15',
+              'cjpillette',
+              'vinnym342',
+              'paigerichards',
+              'Confidenceman02',
+              'melvourne',
+              'C-lockwood',
+              'Samseppiol',
+              'AnnSiapno',
+              'vkalfieri',
+              'dsaleem',
+              'Csthy93',
+              'madzc97'
+]
+
+
+
 class App extends Component {
-  state = {
+  state = JSON.parse(localStorage.getItem('savedData')) || {
     error: null,
+    repoName: null,
     repos: null
   }
+
+
 
   render() {
     const { error, repos } = this.state
     return (
       <div className="App">
+        {
+          names.map((name) => (
+            <li>
+              <a onClick={ () => this.handleClick(name) } style={{cursor: 'pointer'}}>{name}</a>
+            </li>
+          ))
+        }
         { !!error && <p>{ error.message }</p> }
         {
           !!repos ? (
             <RepoNames items={ repos } />
           ) : (
-            'Loading repos...'
+            ''
           )
         }
-      
       </div>
     );
   }
-  componentDidMount() {
-    fetch('https://api.github.com/users/george2kra/repos')
-      .then(res => res.json())
-      .then(repos => {
-        this.setState({ repos })
-      })
-      .catch(error => {
-        this.setState({ error })
-      })
+
+    handleClick = (repoName) => {      
+      fetch(`https://api.github.com/users/${repoName}/repos`)
+        .then(res => res.json())
+        .then(repos => {
+          this.setState({ repoName, repos })
+        })
+        .catch(error => {
+          this.setState({ error })
+        })
   }
+
+  componentDidUpdate() {
+    localStorage.setItem('savedData', JSON.stringify(this.state))
+  }
+
+
+  // componentDidMount() {
+  //   fetch(`https://api.github.com/users/repos`)
+  //     .then(res => res.json())
+  //     .then(repos => {
+  //       this.setState({ repos })
+  //     })
+  //     .catch(error => {
+  //       this.setState({ error })
+  //     })
+  // }
 }
 
 export default App;
